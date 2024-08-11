b6b0917ce4b4a938996277ad4a3468bfbe214fcd
Лев Иванов <leevivanov@mail.ru>
2024-08-11 21:46:24 +0300

    csu.py

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin@ya.ru',
+            email='admin@sky.pro',
             first_name='admin',
-            last_name='ya.ru',
+            last_name='Skypro',
             is_staff=True,
             is_superuser=True
         )
