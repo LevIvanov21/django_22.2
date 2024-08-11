48f159490548a64bb194c34f26a92930fa22f693
Лев Иванов <leevivanov@mail.ru>
2024-08-11 20:44:44 +0300

    database_filter.py

--- project/management/commands/database_filter.py
+++ project/management/commands/database_filter.py
@@ -10,20 +10,20 @@ class Command(BaseCommand):
 
     @staticmethod
     def json_read_categories():
-        with open('catalog/fixtures/category_data.json', 'r', encoding='UTF-8') as category_file:
+        with open('project/fixtures/category_data.json', 'r', encoding='UTF-8') as category_file:
             data = json.load(category_file)
         return data
 
     @staticmethod
     def json_read_products():
-        with open('catalog/fixtures/product_data.json', 'r', encoding='UTF-8') as product_file:
+        with open('project/fixtures/product_data.json', 'r', encoding='UTF-8') as product_file:
             data = json.load(product_file)
         return data
 
     def handle(self, *args, **options):
 
         with connection.cursor() as cur:
-            cur.execute(f'TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE;')
+            cur.execute(f'TRUNCATE TABLE project_category RESTART IDENTITY CASCADE;')
 
         Category.objects.all().delete()
         Product.objects.all().delete()
